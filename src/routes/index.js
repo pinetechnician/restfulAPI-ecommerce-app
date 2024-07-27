146a94f6aa6c1a49e81a39c79c885ef9295648b8
pinetechnician <pilotaadam@gmail.com>
2024-07-27 14:18:33 -0500

    Addition of order routes that let a user see their orders and search orders by orderId

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRoutes = require('./userRoutes');
 const productRoutes = require('./productRoutes');
 const cartRoutes = require('./cartRoutes');
+const orderRoutes = require('./orderRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/cart', cartRoutes);
+router.use('/orders', orderRoutes);
 
 module.exports = router;
